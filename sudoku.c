9eb09844d03317454b0f6b9f17f4675fff446fa6
Your Name <you@example.com>
2024-05-16 04:14:44 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -64,7 +64,7 @@ List* get_adj_nodes(Node* n){
    int columna;
    for (int i = 0 ; i < 9 ; i++){
       for (int j = 0 ; j < 9 ; j++){
-         columna = j
+         columna = j;
          if(n->sudo[i][j] == 0)
          {
             break;
