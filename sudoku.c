fc472f305b50396fc6745d8f5d623179d58875e4
Your Name <you@example.com>
2024-05-16 04:30:47 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -87,7 +87,7 @@ List* get_adj_nodes(Node* n){
       {
          Node *aux = copy(n);
          aux->sudo[fila][columna] = k;
-         if (is_valid(aux))
+         if is_valid(aux)
          {
             pushBack(list,aux);
          }
